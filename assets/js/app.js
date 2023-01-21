6f2709976bbef457561105384c1100a77dacf919
RejoyanIslam <rejoyanislam0014@gmail.com>
2023-01-22 02:20:22 +0600

    upload pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,10 @@
 const indicator = document.getElementById("indicator");
 
-
-
 window.onscroll = (e) => {
-  const pageScroll=window.screenY
-  const totalPageHight = document.body.scrollHeight; 
+  const pageScroll = window.screenY;
+  const totalPageHight = document.body.scrollHeight;
   const deviceHeight = window.innerHeight;
   // +95,is default value of scrollbar
-const percent =
-  (pageScroll/ (totalPageHight- deviceHeight + 95)*100);
-indicator.style.width=`${percent}%`
+  const percent = (pageScroll / (totalPageHight - deviceHeight + 95)) * 100;
+  indicator.style.width = `${percent}%`;
 };
-
-
-
-
-
-
-
-
-
